49139387270efb9e6405d6d6483c50d8c41270de
Edwin RJ <edwinrj1975@gmail.com>
2026-02-12 22:04:05 +0530

    Time: 46 ms (52.96%) | Memory: 73.5 MB (69.54%) - LeetSync

---
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public int LongestConsecutive(int[] nums) {
+          var set = new HashSet<int>(nums);
+        int longest = 0;
+
+        foreach (int num in set)
+        {
+            if (!set.Contains(num - 1))  // Start of sequence
+            {
+                int current = num;
+                int streak = 1;
+
+                while (set.Contains(current + 1))
+                {
+                    current++;
+                    streak++;
+                }
+
+                longest = Math.Max(longest, streak);
+            }
+        }
+
+        return longest;
+    }
+}
