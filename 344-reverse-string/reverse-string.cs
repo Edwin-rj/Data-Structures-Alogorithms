ae0dd57b58e2b6628ad01bb2fe87c73b34bcba48
Edwin RJ <edwinrj1975@gmail.com>
2026-07-26 22:08:16 +0530

    Time: 0 ms (100.00%) | Memory: 64.4 MB (94.08%) - LeetSync

--- 344-reverse-string/reverse-string.cs
+++ 344-reverse-string/reverse-string.cs
@@ -1,12 +1,16 @@
 public class Solution {
     public void ReverseString(char[] s) {
         
-        int length = s.Length-1;
-        for (int i = 0; i < s.Length / 2; i++)
+        int left = 0;
+        int right = s.Length - 1;
+        while(left<right)
         {   
-            var temp = s[i];
-            s[i] = s[length - i];
-            s[length - i] = temp;  
+            var temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;  
+
+            left++;
+            right--;
         }
     }
 }
