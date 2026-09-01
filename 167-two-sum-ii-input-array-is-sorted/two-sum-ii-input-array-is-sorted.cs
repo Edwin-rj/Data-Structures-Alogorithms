a13eee1d51a1f7af33d18e1b00f1c44533fb69a3
Edwin RJ <edwinrj1975@gmail.com>
2026-09-01 21:27:40 +0530

    Time: 0 ms (100.00%) | Memory: 49.3 MB (81.33%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cs
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cs
@@ -1,31 +1,31 @@
 public class Solution {
     public int[] TwoSum(int[] numbers, int target) {
-        
 
-        //numbers = [-50, -30, -10, -3, 0, 4, 8, 15, 22, 31, 45, 60, 75, 90]
-        //target = 37
+    int leftPointer = 0 ;
+    int rightPointer = numbers.Length -1;
+    int value;
 
-        int left  = 0;
-        int right = numbers.Length - 1;
-        int total = 0; 
-        while(left <  right)
+    while(leftPointer <  rightPointer)
+    {
+     
+        value = (numbers[leftPointer] + numbers[rightPointer]);
+
+
+        if(value  >  target)
         {
-            total = numbers[left] + numbers[right];
-        
-            if(total < target)
-            {
-                left++;
-            }
-            else if(total > target)
-            {
-                right--;
-            }
-            else
-            {
-                return new int[] {left +1 ,right+1};
-            }
+            rightPointer --;
         }
+        else if(value  <  target)
+        {
+            leftPointer ++;             
+        }
+        else
+        {
+            return new int[] {leftPointer+1 , rightPointer+1};
+        }
+    
+    }
 
-        return null;
+    return null;
     }
 }
